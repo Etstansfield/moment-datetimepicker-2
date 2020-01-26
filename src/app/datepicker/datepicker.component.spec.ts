93bc2631e8e9374714640dc25b8d04e830ddd7f9
Edward <etstansfield@hotmail.com>
2020-01-26 13:22:38 +0000

    add tests - now to 100% coverage

--- src/app/datepicker/datepicker.component.spec.ts
+++ src/app/datepicker/datepicker.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DatepickerComponent } from './datepicker.component';
+import * as moment from 'moment';
 
 describe('DatepickerComponent', () => {
   let component: DatepickerComponent;
@@ -19,7 +20,46 @@ describe('DatepickerComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('+++ Should create +++', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`+++ Should have default date as today (${moment().date()}/${moment().month() + 1}/${moment().year()}) +++`, () => {
+    const curDate = moment();
+    expect(
+      component.startDateTime.date() === curDate.date() &&
+      component.startDateTime.month() === curDate.month() &&
+      component.startDateTime.year() === curDate.year() &&
+      component.startDateTime.hour() === curDate.hour() &&
+      component.startDateTime.minute() === curDate.minute() // ! - Don't test down any further - not needed
+    ).toBeTruthy();
+  });
+
+  it(`+++ Should alter the default date to the 20th correctly +++`, () => {
+    component.pickDate(20);
+    expect(component.pickedDateTime.date() === 20).toBeTruthy();
+  });
+
+  it(`+++ Should not alter the date to the 32nd +++`, () => {
+    component.pickDate(32);
+    expect(component.pickedDateTime.date() === 32).toBeFalsy();
+  });
+
+  it(`+++ Should not alter the date to the 0th +++`, () => {
+    component.pickDate(0);
+    expect(component.pickedDateTime.date() === 0).toBeFalsy();
+  });
+
+  it(`+++ Should detect the current day of the month correctly (${moment().date()}) +++`, () => {
+    expect(component.isCurrentDate(moment().date())).toBeTruthy();
+  });
+
+  it(`+++ Should not overwrite our custom start date to the current date +++`, () => {
+    const pastDateTime = moment('12/12/1999', 'DD/MM/YYYY');  // lets party like it's 1999... again
+    component.startDateTime = moment(pastDateTime);
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(pastDateTime.isSame(component.startDateTime)).toBeTruthy();
+  });
+
 });
